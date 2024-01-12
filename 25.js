e8df9be383692c865ebb6fa822ea4265bcd9d791
Smoke <smouk2003kz@mail.ru>
2024-01-12 22:19:39 +0300

    redfull2

--- 25.js
+++ 25.js
@@ -4,6 +4,13 @@ function drawChess() {
   let block;
   let flag = true;
 
+   let figure = {
+        0 : ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-150px -16px','-372px -17px', '-484px -22px','-263px -19px'],
+        1 : ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px','-595px -19px', '-595px -19px','-595px -19px'],
+        6 : ['-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px','-595px -116px', '-595px -116px','-595px -116px'],
+        7 : ['-263px -116px', '-484px -116px', '-372px -116px', '-150px -116px', '-150px -116px','-372px -116px', '-484px -116px','-263px -116px'],
+    };
+
   /*вроде це будет цикл теперь то что фор да да я запомнил что начинается с нуля евгений(великий человек если что) по горезонтали*/
   for (let i = 0; i < 8; i++) {
    
@@ -16,6 +23,12 @@ function drawChess() {
       if (flag) block.className = 'block black';
      
        else block.className = 'block white';
+
+       if (figure[i]!==undefined && figure[i][j]!==undefined){
+       	block.style.backgroundImage ='url(red.png)';
+       		block.style.backgroundPosition = figure[i][j];
+
+       }
      
       mainblock.appendChild(block);
       flag = !flag;
